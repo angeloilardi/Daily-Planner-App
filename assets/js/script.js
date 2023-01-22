a18f1226a504cbb8de6de94e4252a231a8cf7ae7
Angelo Ilardi <angeloilardi@Angelos-MacBook-Air.local>
2023-01-22 13:31:12 +0000

    added function that display saved events after refreshing the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,34 +3,38 @@ let currentDay = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").text(currentDay);
 console.log(currentDay);
 
-
-
+// set the start time for the calendar
 let startTime = moment('7am', 'hA');
-console.log(startTime);
 
+// set the finish time for the calendar
 let finishTime =  moment('7pm', 'hA')
 
 // add one row for each time block
 let i = startTime;
 
-while (moment(i).isSameOrBefore(finishTime)){
-    console.log('added time');
+function createTimeBlocks() {
+    while (moment(i).isSameOrBefore(finishTime)){
+    //create a row for each hour
     let timeBlock = $("<div>").addClass("row");
+    //display the hour
     let timeDisplay = $("<div>")
     .addClass("col-2 col-s-3 hour")
     .text(moment(i).format("hA"));
+    // add textarea
     let eventDetails = $("<textarea>")
     .addClass("col-8");
-    let saveBtn = $("<button>")
-    .addClass("col-2 saveBtn");
+    // add save button for each row
+    let saveBtn = $("<button>").addClass("col-2 saveBtn");
     let saveicon = $("<i></i>").addClass("fa-regular fa-floppy-disk");
     saveBtn.append(saveicon);
+    // append each time block to the container
     $(".container").append(timeBlock);
     timeBlock.append(timeDisplay, eventDetails,saveBtn);
+    // increase by one hour and create the next time block
     i = moment(i).add(1, 'h');
 }
 
-// function that checks if the if each timr block is in the past, present or future and giveds a different background accordingly
+// function that checks if the if each time block is in the past, present or future and sets a different background accordingly
 $(".hour").each(function(){
     if (moment($(this).text(), "hA").isBefore(moment(), "hour")){
         $(this).siblings().addClass("past");
@@ -44,18 +48,48 @@ $(".hour").each(function(){
 //add event listener to the save buttons
 
 $(".saveBtn").on("click", function(){
-    let savedDay = moment().format("DD MM YY");
-    console.log(savedDay);
-    let savedHour = $(this).siblings(".hour").text();
-    console.log(savedHour);
-    savedEvent = $(this).siblings("textarea").val();
-    console.log(savedEvent);
+    //the saved event list is parsed as an object
+    let currentEventList = JSON.parse(localStorage.getItem("updatedEventList"));
+    console.log(currentEventList);
+    // if there are no entries in the list, the new current list is set to an empty array
+    if (currentEventList === null) currentEventList = [];
+    // get the current day + hor of the time block
+    let eventDate = `${moment().format("DD MM YY")} ${$(this).siblings(".hour").text()}`
+    // get the calendar activity's iput
+    let savedEventDetail = $(this).siblings("textarea").val();
+    // check if there's already an entry for that day/time
+    let existingEntryIndex = currentEventList.findIndex(el => el.eventDate === eventDate)
+    console.log(existingEntryIndex);
+    // if there's no previous entry, add a new object witth the event details in the array
+    if (existingEntryIndex === -1){
+        let newEvent = {
+            eventDate: eventDate,
+            eventActivity: savedEventDetail
+        }
+        localStorage.setItem("newEvent", JSON.stringify(newEvent));
+        currentEventList.push(newEvent);
+        //else just update the existing entry
+    } else currentEventList[existingEntryIndex].eventActivity = savedEventDetail;
+    // finally update the event list object
+    localStorage.setItem("updatedEventList", JSON.stringify(currentEventList));
+});}
+
+
+// function that displays all the saved events currently in the local storage
+function displaySavedEvents() {
+    $("textarea").each(function(){
+        timeBlock = `${moment().format("DD MM YY")} ${$(this).siblings(".hour").text()}`;
+    let listToDisplay = JSON.parse(localStorage.getItem("updatedEventList"));
+    if(listToDisplay !== null){
+        let existingIndex = listToDisplay.findIndex(e => e.eventDate === timeBlock);
+console.log(existingIndex);
+        if (existingIndex !== -1){
+            $(this).text(listToDisplay[existingIndex].eventActivity);
+            console.log($(this).val());
+        } else $(this).val("");
+    }
 })
+};
 
-// console.log(moment(currentDay + $(".hour").text(), "DD MM YY hA").format("DD MM YY hA"));
-
-let savedDay = moment().format("DD MM YY");
-console.log(savedDay);
-let savedHour = $(".saveBtn").siblings(".hour").text();
-console.log(savedHour);
-savedEvent = $(".saveBtn").siblings("textarea").val();
+createTimeBlocks();
+displaySavedEvents();
